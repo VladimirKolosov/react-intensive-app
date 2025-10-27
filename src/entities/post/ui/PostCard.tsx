1bc5518e55c625520204101ea57ce17bfd3b3348
Vladimir Kolosov <tr4nk.kol@gmail.com>
2025-10-27 13:17:10 +0300

    Cоздание списка постов и передача данных через props

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -1,5 +1,11 @@
-function PostCard({}) {
-  return <div></div>;
+function PostCard({ postName, postDescrpiption, idPost }) {
+  return (
+    <div>
+      <h1>{postName}</h1>
+      <h4>{idPost}</h4>
+      <p>{postDescrpiption}</p>
+    </div>
+  );
 }
 
 export default PostCard;
--- src/widgets/PostList/PostList.tsx
+++ src/widgets/PostList/PostList.tsx
@@ -1,7 +1,20 @@
 import PostCard from '../../entities/post/ui/PostCard';
 
 function PostList() {
-  return <></>;
+  return (
+    <>
+      <PostCard
+        postName="История"
+        postDescrpiption="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio, similique!"
+        idPost="1"
+      />
+      <PostCard
+        postName="История 2"
+        postDescrpiption="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio, similique!"
+        idPost="2"
+      />
+    </>
+  );
 }
 
 export default PostList;
